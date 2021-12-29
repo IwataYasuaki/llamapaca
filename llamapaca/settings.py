dc361fb94b9bda5104747dc980e1f17a1b5064a5
Yasuaki Iwata <test@example.com>
2021-12-29 10:11:26 +0900

    modified allowed host to *

--- llamapaca/settings.py
+++ llamapaca/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fnu+x2opz#0zyj+ms76y*+__k-zcy$9rs0+clznvg*t3v*lhmr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
