1de1c6f4470480f01085dc154d151d0467d0286f
Yasuaki Iwata <test@example.com>
2022-01-09 23:09:26 +0900

    Heroku用設定の追加

--- llamapaca/settings.py
+++ llamapaca/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -171,5 +171,4 @@ if not DEBUG:
     django_heroku.settings(locals())
     SECRET_KEY = os.environ['SECRET_KEY']
 
-SECRET_KEY = os.environ['SECRET_KEY']
 
