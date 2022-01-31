519f180670d9a13229e40a750e3320862ac6ef94
Yasuaki Iwata <test@example.com>
2022-01-31 20:31:31 +0900

    テスト実装。ログイン、ユーザー登録、メンバー管理。

--- bulklot/tests.py
+++ bulklot/tests.py
@@ -1,3 +1,171 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import Member
 
-# Create your tests here.
+class MemberListViewTests(TestCase):
+    def test_member(self):
+        """
+        自分のメンバーがすべて表示される。
+        """
+        # テストユーザ
+        user = User.objects.create_user('tester', password='llamapaca')
+        other = User.objects.create_user('other', password='llamapaca')
+
+        # テストメンバー
+        member1 = Member.objects.create(
+            owner=user, 
+            name='test1', 
+            tmgbc_id='11111111', 
+            tmgbc_password='11111111',
+        )
+        member2 = Member.objects.create(
+            owner=user, 
+            name='test2', 
+            tmgbc_id='22222222', 
+            tmgbc_password='22222222',
+        )
+        Member.objects.create(
+            owner=other, # other
+            name='test3', 
+            tmgbc_id='33333333', 
+            tmgbc_password='33333333',
+        )
+
+        # ログイン
+        self.client.force_login(user)
+
+        # メンバーリスト画面をGET
+        response = self.client.get(reverse('bulklot:member_list'))
+
+        # assert
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'メンバー')
+        self.assertQuerysetEqual(
+            list(response.context['member_list']),
+            [member1, member2],
+        )
+
+
+class MemberCreateViewTests(TestCase):
+    def test_member(self):
+        """
+        メンバーを新規登録する。
+        """
+        # テストユーザ
+        user = User.objects.create_user('tester', password='llamapaca')
+
+        # POSTパラメータ
+        name='test1' 
+        tmgbc_id='11111111' 
+        tmgbc_password='11111111'
+        param = {'name': name, 'tmgbc_id': tmgbc_id, 'tmgbc_password': tmgbc_password}
+
+        # ログイン
+        self.client.force_login(user)
+
+        # 登録前はメンバーが存在しないこと
+        members = Member.objects.filter(name=name)
+        self.assertEqual(members.count(), 0)
+
+        # メンバー登録リクエストをPOST
+        response = self.client.post(reverse('bulklot:member_create'), param, follow=True)
+
+        # assert
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'メンバー')
+        self.assertContains(response, name)
+        members = Member.objects.filter(name=name)
+        self.assertEqual(members.count(), 1)
+
+class MemberUpdateViewTests(TestCase):
+    def test_member(self):
+        """
+        メンバーを変更する。
+        """
+        # テストユーザ
+        user = User.objects.create_user('tester', password='llamapaca')
+
+        # テストメンバー
+        name='test1' 
+        tmgbc_id='11111111'
+        tmgbc_password='11111111'
+
+        member = Member.objects.create(
+            owner=user, 
+            name=name, 
+            tmgbc_id=tmgbc_id, 
+            tmgbc_password=tmgbc_password,
+        )
+        
+        # POSTパラメータ
+        tmgbc_id_new='22222222'
+        param = {'name': name, 'tmgbc_id': tmgbc_id_new, 'tmgbc_password': tmgbc_password}
+
+        # ログイン
+        self.client.force_login(user)
+
+        # メンバー変更リクエストをPOST
+        response = self.client.post(
+            reverse('bulklot:member_update', kwargs={'pk': member.id}),
+            param,
+            follow=True,
+        )
+
+        # assert
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'メンバー')
+        self.assertContains(response, tmgbc_id_new)
+        member.refresh_from_db()
+        self.assertEqual(member.tmgbc_id, tmgbc_id_new)
+        self.assertQuerysetEqual(
+            list(response.context['member_list']),
+            [member],
+        )
+
+class MemberDeleteViewTests(TestCase):
+    def test_member(self):
+        """
+        メンバーを削除する。
+        """
+        # テストユーザ
+        user = User.objects.create_user('tester', password='llamapaca')
+
+        # テストメンバー
+        name='test1' 
+        tmgbc_id='11111111'
+        tmgbc_password='11111111'
+
+        member = Member.objects.create(
+            owner=user, 
+            name=name, 
+            tmgbc_id=tmgbc_id, 
+            tmgbc_password=tmgbc_password,
+        )
+
+        # POSTパラメータ
+        param = {'name': name, 'tmgbc_id': tmgbc_id, 'tmgbc_password': tmgbc_password}
+
+        # ログイン
+        self.client.force_login(user)
+
+        # 削除前はメンバーが存在すること
+        members = Member.objects.filter(name=name)
+        self.assertEqual(members.count(), 1)
+
+        # メンバー削除リクエストをPOST
+        response = self.client.post(
+            reverse('bulklot:member_delete', kwargs={'pk': member.id}),
+            param,
+            follow=True,
+        )
+
+        # assert
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'メンバー')
+        self.assertNotContains(response, name)
+        members = Member.objects.filter(name=name)
+        self.assertEqual(members.count(), 0)
+
+
+
--- registration/tests.py
+++ registration/tests.py
@@ -1,3 +1,40 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class RegistrationTests(TestCase):
+    def test_login(self):
+        """
+        ログイン
+        """
+        response = self.client.get(reverse('registration:login'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'ログイン')
+
+        username = 'tester'
+        password = 'llamapaca'
+        user = User.objects.create_user(username, password=password)
+        param = {'username': username, 'password': password}
+        response = self.client.post(reverse('registration:login'), param, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'ホーム')
+        self.assertContains(response, 'ユーザー: ' + username)
+        
+    def test_signup(self):
+        """
+        ユーザー登録
+        """
+        response = self.client.get(reverse('registration:signup'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'ユーザー登録')
+
+        username = 'tester'
+        password = 'llamapaca'
+        param = {'username': username, 'password1': password, 'password2': password}
+        response = self.client.post(reverse('registration:signup'), param, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'ホーム')
+        self.assertContains(response, 'ユーザー: ' + username)
+
+
+
